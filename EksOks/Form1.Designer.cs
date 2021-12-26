b485ae33672473d391e29b6b16aa4a5674237e50
AMI <AMI@DESKTOP-FU6APS6>
2021-12-27 00:27:48 +0100

    Add project files.

---
+++ EksOks/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace EksOks
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.b11 = new System.Windows.Forms.Button();
+			this.b21 = new System.Windows.Forms.Button();
+			this.b31 = new System.Windows.Forms.Button();
+			this.b32 = new System.Windows.Forms.Button();
+			this.b22 = new System.Windows.Forms.Button();
+			this.b12 = new System.Windows.Forms.Button();
+			this.b33 = new System.Windows.Forms.Button();
+			this.b23 = new System.Windows.Forms.Button();
+			this.b13 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.XWin = new System.Windows.Forms.Label();
+			this.OWin = new System.Windows.Forms.Label();
+			this.Draw = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// b11
+			// 
+			this.b11.BackColor = System.Drawing.Color.Plum;
+			this.b11.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b11.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b11.FlatAppearance.BorderSize = 2;
+			this.b11.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b11.Location = new System.Drawing.Point(12, 12);
+			this.b11.Name = "b11";
+			this.b11.Size = new System.Drawing.Size(69, 68);
+			this.b11.TabIndex = 0;
+			this.b11.UseVisualStyleBackColor = false;
+			this.b11.Click += new System.EventHandler(this.button_click);
+			// 
+			// b21
+			// 
+			this.b21.BackColor = System.Drawing.Color.Plum;
+			this.b21.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b21.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b21.FlatAppearance.BorderSize = 2;
+			this.b21.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b21.Location = new System.Drawing.Point(12, 86);
+			this.b21.Name = "b21";
+			this.b21.Size = new System.Drawing.Size(69, 68);
+			this.b21.TabIndex = 1;
+			this.b21.UseVisualStyleBackColor = false;
+			this.b21.Click += new System.EventHandler(this.button_click);
+			// 
+			// b31
+			// 
+			this.b31.BackColor = System.Drawing.Color.Plum;
+			this.b31.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b31.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b31.FlatAppearance.BorderSize = 2;
+			this.b31.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b31.Location = new System.Drawing.Point(12, 160);
+			this.b31.Name = "b31";
+			this.b31.Size = new System.Drawing.Size(69, 68);
+			this.b31.TabIndex = 2;
+			this.b31.UseVisualStyleBackColor = false;
+			this.b31.Click += new System.EventHandler(this.button_click);
+			// 
+			// b32
+			// 
+			this.b32.BackColor = System.Drawing.Color.Plum;
+			this.b32.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b32.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b32.FlatAppearance.BorderSize = 2;
+			this.b32.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b32.Location = new System.Drawing.Point(87, 160);
+			this.b32.Name = "b32";
+			this.b32.Size = new System.Drawing.Size(69, 68);
+			this.b32.TabIndex = 5;
+			this.b32.UseVisualStyleBackColor = false;
+			this.b32.Click += new System.EventHandler(this.button_click);
+			// 
+			// b22
+			// 
+			this.b22.BackColor = System.Drawing.Color.Plum;
+			this.b22.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b22.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b22.FlatAppearance.BorderSize = 2;
+			this.b22.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b22.Location = new System.Drawing.Point(87, 86);
+			this.b22.Name = "b22";
+			this.b22.Size = new System.Drawing.Size(69, 68);
+			this.b22.TabIndex = 4;
+			this.b22.UseVisualStyleBackColor = false;
+			this.b22.Click += new System.EventHandler(this.button_click);
+			// 
+			// b12
+			// 
+			this.b12.BackColor = System.Drawing.Color.Plum;
+			this.b12.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b12.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b12.FlatAppearance.BorderSize = 2;
+			this.b12.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b12.Location = new System.Drawing.Point(87, 12);
+			this.b12.Name = "b12";
+			this.b12.Size = new System.Drawing.Size(69, 68);
+			this.b12.TabIndex = 3;
+			this.b12.UseVisualStyleBackColor = false;
+			this.b12.Click += new System.EventHandler(this.button_click);
+			// 
+			// b33
+			// 
+			this.b33.BackColor = System.Drawing.Color.Plum;
+			this.b33.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b33.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b33.FlatAppearance.BorderSize = 2;
+			this.b33.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b33.Location = new System.Drawing.Point(162, 160);
+			this.b33.Name = "b33";
+			this.b33.Size = new System.Drawing.Size(69, 68);
+			this.b33.TabIndex = 8;
+			this.b33.UseVisualStyleBackColor = false;
+			this.b33.Click += new System.EventHandler(this.button_click);
+			// 
+			// b23
+			// 
+			this.b23.BackColor = System.Drawing.Color.Plum;
+			this.b23.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b23.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b23.FlatAppearance.BorderSize = 2;
+			this.b23.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b23.Location = new System.Drawing.Point(162, 86);
+			this.b23.Name = "b23";
+			this.b23.Size = new System.Drawing.Size(69, 68);
+			this.b23.TabIndex = 7;
+			this.b23.UseVisualStyleBackColor = false;
+			this.b23.Click += new System.EventHandler(this.button_click);
+			// 
+			// b13
+			// 
+			this.b13.BackColor = System.Drawing.Color.Plum;
+			this.b13.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+			this.b13.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+			this.b13.FlatAppearance.BorderSize = 2;
+			this.b13.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.b13.Location = new System.Drawing.Point(162, 12);
+			this.b13.Name = "b13";
+			this.b13.Size = new System.Drawing.Size(69, 68);
+			this.b13.TabIndex = 6;
+			this.b13.UseVisualStyleBackColor = false;
+			this.b13.Click += new System.EventHandler(this.button_click);
+			// 
+			// button10
+			// 
+			this.button10.BackColor = System.Drawing.Color.Transparent;
+			this.button10.Location = new System.Drawing.Point(12, 271);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(71, 19);
+			this.button10.TabIndex = 9;
+			this.button10.Text = "New Game";
+			this.button10.UseVisualStyleBackColor = false;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// button11
+			// 
+			this.button11.Location = new System.Drawing.Point(89, 271);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(71, 19);
+			this.button11.TabIndex = 10;
+			this.button11.Text = "Reset";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// button12
+			// 
+			this.button12.Location = new System.Drawing.Point(240, 271);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(71, 19);
+			this.button12.TabIndex = 11;
+			this.button12.Text = "Exit";
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// XWin
+			// 
+			this.XWin.AutoSize = true;
+			this.XWin.Location = new System.Drawing.Point(276, 40);
+			this.XWin.Name = "XWin";
+			this.XWin.Size = new System.Drawing.Size(35, 13);
+			this.XWin.TabIndex = 12;
+			this.XWin.Text = "label1";
+			// 
+			// OWin
+			// 
+			this.OWin.AutoSize = true;
+			this.OWin.Location = new System.Drawing.Point(276, 114);
+			this.OWin.Name = "OWin";
+			this.OWin.Size = new System.Drawing.Size(35, 13);
+			this.OWin.TabIndex = 13;
+			this.OWin.Text = "label2";
+			// 
+			// Draw
+			// 
+			this.Draw.AutoSize = true;
+			this.Draw.Location = new System.Drawing.Point(276, 188);
+			this.Draw.Name = "Draw";
+			this.Draw.Size = new System.Drawing.Size(35, 13);
+			this.Draw.TabIndex = 14;
+			this.Draw.Text = "label3";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.DodgerBlue;
+			this.ClientSize = new System.Drawing.Size(329, 299);
+			this.Controls.Add(this.Draw);
+			this.Controls.Add(this.OWin);
+			this.Controls.Add(this.XWin);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.b33);
+			this.Controls.Add(this.b23);
+			this.Controls.Add(this.b13);
+			this.Controls.Add(this.b32);
+			this.Controls.Add(this.b22);
+			this.Controls.Add(this.b12);
+			this.Controls.Add(this.b31);
+			this.Controls.Add(this.b21);
+			this.Controls.Add(this.b11);
+			this.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "XO";
+			this.TransparencyKey = System.Drawing.Color.Transparent;
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button b11;
+		private System.Windows.Forms.Button b21;
+		private System.Windows.Forms.Button b31;
+		private System.Windows.Forms.Button b32;
+		private System.Windows.Forms.Button b22;
+		private System.Windows.Forms.Button b12;
+		private System.Windows.Forms.Button b33;
+		private System.Windows.Forms.Button b23;
+		private System.Windows.Forms.Button b13;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+		private System.Windows.Forms.Label XWin;
+		private System.Windows.Forms.Label OWin;
+		private System.Windows.Forms.Label Draw;
+	}
+}
+
---
+++ EksOks/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EksOks
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		public int player = 2;
+		public int move = 0;
+		public int s1 = 0;
+		public int s2 = 0;
+		public int sn = 0;
+
+		private void button_click(object sender, EventArgs e)
+		{
+			Button button = (Button) sender;
+			if (button.Text == "")
+			{
+				if (player % 2 == 0)
+				{
+					button.Text = "X";
+					player++;
+					move++;
+				}
+				else
+				{
+					button.Text = "O";
+					player++;
+					move++;
+				}
+				if (tie() == true)
+				{
+					MessageBox.Show("GAME IS A TIE!", "I N F O");
+					sn++;
+					Draw.Text = "Tie: " + sn;
+				}
+				if (winner() == true)
+				{
+					if (button.Text == "X")
+					{
+						MessageBox.Show("WINNER IS PLAYER X ! ! !", "WE HAVE A WINNER ! ! !");
+						s1++;
+						XWin.Text = "X: " + s1;
+					}
+					else
+					{
+						MessageBox.Show("WINNER IS PLAYER O ! ! !", "WE HAVE A WINNER ! ! !");
+						s2++;
+						OWin.Text = "O: " + s2;
+					}					
+				}
+			}
+		}
+		public bool tie()
+		{
+			if (move == 9 && winner() == false)
+				return true;
+			else
+			{
+				return false;
+			}
+		}
+		public bool winner()
+		{
+			if (b11.Text == b12.Text && b12.Text == b13.Text && b11.Text != "" ||
+					b21.Text == b22.Text && b22.Text == b23.Text && b21.Text != "" ||
+					b31.Text == b32.Text && b32.Text == b33.Text && b31.Text != "" ||
+					b11.Text == b21.Text && b21.Text == b31.Text && b11.Text != "" ||
+					b12.Text == b22.Text && b22.Text == b32.Text && b12.Text != "" ||
+					b13.Text == b23.Text && b23.Text == b33.Text && b13.Text != "" ||
+					b11.Text == b22.Text && b22.Text == b33.Text && b11.Text != "" ||
+					b13.Text == b22.Text && b22.Text == b31.Text && b13.Text != "")
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		void NewGame()
+		{
+			player = 2;
+			move = 0;
+			b11.Text = b12.Text = b13.Text = b21.Text = b22.Text = b23.Text = b31.Text = b32.Text = b33.Text = "";
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			NewGame();
+		}
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			s1 = s2 = sn = 0;
+			XWin.Text = "X: 0";
+			OWin.Text = "O: 0";
+			Draw.Text = "Tie: 0";
+			NewGame();
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+			Close();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			XWin.Text = "X: " + s1;
+			OWin.Text = "O: " + s2;
+			Draw.Text = "Tie: " + sn;
+		}
+	}
+}
